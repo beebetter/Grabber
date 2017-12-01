ae1cfa3734688d00e43dc54ebb4cab1fda2361d9
beeBetter <artem.ogarkov@gmail.com>
2017-12-01 11:50:07 +0200

    add new grabber

--- src/main/java/Grabber.java
+++ src/main/java/Grabber.java
@@ -1,49 +1,79 @@
 /**
  * Created by Artem on 25.11.2017.
  */
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Grabber {
-    public void grab() {
-        String urlHead = "https://ru.osvita.ua/vnz/guide/search-17-0-0-41-292-";
-        String urlTail = ".html";
-        String all = "";
-        for (int i = 0; i <= 11; i++) {
-            grab(urlHead + i*25 + urlTail);
-        }
-    }
-    public void grab(String url) {
+    public Set<String> pageParse(String url){
         Document doc = null;
+        Set<String> emails = new HashSet<String>();
         try {
             doc = Jsoup.connect(url).userAgent("Mozilla").timeout(10 * 1000).get();
-            Elements AllLinks = doc.select("a[href]");
+            String cssTable = "#okrArea > div:nth-child(1)";
+            String info = doc.select(cssTable).text();
+            info = info.replace(" [ at ] ", "@");
+            Regex regex = new Regex();
+            emails = regex.findEmails(info);
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+        return emails;
+    }
+
+    public Set<String> vnzFinder(String url) {
+        Document doc = null;
+        Set<String> emails = new HashSet<String>();
+        try {
+            doc = Jsoup.connect(url).userAgent("Mozilla").timeout(10 * 1000).get();
+            String region = "#menu-nav > li.current > a";
+            System.out.print(doc.select(region).text() + " has ");
+            String cssTable = "#okrArea";
+            Elements AllLinks = doc.select(cssTable).select("a[href]");
             Set<Element> links = new HashSet<Element>();
-            Set<String> pages = new HashSet<String>();
             for (Element link : AllLinks) {
                 links.add(link);
             }
             for (Element link : links) {
-                String tmpLink = link.attr("href");
-                if (tmpLink.length() > 11 && tmpLink.length() <= 20 && tmpLink.contains("/vnz/guide/")) {
-                    pages.add(tmpLink);
+                String tmpLink = link.toString();
+                tmpLink = tmpLink.substring(tmpLink.indexOf("href=\".")+8, tmpLink.indexOf(">") - 1);
+                if (tmpLink.contains(".html")) {
+                    emails.addAll(pageParse("http://www.vstup.info/2017/" + tmpLink));
                 }
             }
-            String cssTable = "#center > article > table > tbody > tr > td.vmiddle";
-            //Regex regex = new Regex();
-            for (String page : pages) {
-                doc = Jsoup.connect("https://ru.osvita.ua" + page).userAgent("Mozilla").timeout(10 * 1000).get();
-                System.out.println(doc.select(cssTable).text());
-                //regex.findEmails(doc.select(cssTable).text());
-            }
-
+            System.out.println(emails.size() + " emails.");
         } catch (IOException e) {
+            //e.printStackTrace();
+        }
+        return emails;
+    }
+
+    public void regionFinder() {
+        Set<String> emails = new HashSet<String>();
+        for (int i = 3; i <= 26; i++) {//27 - Kyiv
+            String url = String.format("http://www.vstup.info/2017/i2017o%d.html", i);
+            emails.addAll(vnzFinder(url));
+        }
+        System.out.println(String.format("Total number of emails: %d", emails.size()));
+        try {
+            String path = String.format("All regions without Kyiv (%d emails).txt", emails.size());
+            File f = new File(path);
+            f.createNewFile();
+            PrintWriter out = new PrintWriter(path);
+            for (String email: emails) {
+                out.write(email + "\n");
+            }
+            out.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -5,6 +5,9 @@ public class Run {
     public static void  main(String ... args) {
         //Grabber_EnicInUA grabber = new Grabber_EnicInUA();
         Grabber grabber = new Grabber();
-        grabber.grab();
+        //grabber.grab();
+        //grabber.pageParse("http://www.vstup.info/2017/i2017i318.html");
+        //grabber.vnzFinder("http://www.vstup.info/2017/i2017o12.html");
+        grabber.regionFinder();
     }
 }
